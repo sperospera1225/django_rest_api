4b18629dff0612af715a1e470700b1d9d58a6d7e
MinseonKim <sperospera1225@seoultech.ac.kr>
2022-07-22 18:34:22 +0900

    Update settings.py

--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-iw+*@3uh#0!(p%4aorwj7jka9p0slcbhyq42oe9radec&svi(k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
